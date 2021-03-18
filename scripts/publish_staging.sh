6c67e0aa2157684e34a8dcbc6f39101de763af22
mikieribeiro <mikie.ribeiro@aptoide.com>
2021-03-18 10:27:54 +0000

    [APPC-2113/update dev host] - removed "debug" from core an proxy

--- scripts/publish_staging.sh
+++ scripts/publish_staging.sh
@@ -5,5 +5,5 @@ export BUILD_NAME_SUFFIX=".staging"
 export BUILD_TYPE_NAME="release"
 
 ./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean :appcoins-adyen:clean :communication:clean :appcoins-lifecycle:clean :appcoins-core:clean :appcoins-contract-proxy:clean
-./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble :appcoins-adyen:assemble :communication:assemble :appcoins-lifecycle:assemble  :appcoins-core:assembleDebug :appcoins-contract-proxy:assembleDebug
+./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble :appcoins-adyen:assemble :communication:assemble :appcoins-lifecycle:assemble  :appcoins-core:assemble :appcoins-contract-proxy:assemble
 ./gradlew publish bintrayUpload
