25187b2e080f5cf2179b6f15fe8ce4622d130c47
Joao Raimundo <jotaraimundo@gmail.com>
2021-04-30 15:00:53 +0100

    APPC-2322 updated gradle files for publish in new maven repository. Upgradeed version to 0.6.7.0(111) to be easier to identify this change

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,4 +5,4 @@ export BUILD_TYPE_NAME="release"
 
 ./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean :appcoins-adyen:clean :communication:clean :appcoins-lifecycle:clean :appcoins-core:clean :appcoins-contract-proxy:clean
 ./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble :appcoins-adyen:assemble :communication:assemble :appcoins-lifecycle:assemble  :appcoins-core:assemble :appcoins-contract-proxy:assemble
-./gradlew publish bintrayUpload
+./gradlew publish
