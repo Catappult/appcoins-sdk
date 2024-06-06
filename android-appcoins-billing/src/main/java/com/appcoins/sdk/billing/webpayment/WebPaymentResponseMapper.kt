cbc6486909ba98a77e476ab99d1c0a28aef6940b
Sergiy Khomyn <sergiy.khomyn@aptoide.com>
2024-06-06 17:09:24 +0100

    Add analytics for web payment url.

--- android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/webpayment/WebPaymentResponseMapper.kt
+++ android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/webpayment/WebPaymentResponseMapper.kt
@@ -1,13 +1,14 @@
 package com.appcoins.sdk.billing.webpayment
 
+import com.appcoins.sdk.billing.helpers.WalletUtils
 import com.appcoins.sdk.billing.service.RequestResponse
 import com.appcoins.sdk.billing.utils.ServiceUtils.isSuccess
 import org.json.JSONObject
 
 class WebPaymentResponseMapper {
     fun map(response: RequestResponse): WebPaymentResponse {
-        /*WalletUtils.getSdkAnalytics()
-            .sendCallBackendPayflowEvent(response.responseCode, response.response)*/
+        WalletUtils.getSdkAnalytics()
+            .sendCallBackendWebPaymentUrlEvent(response.responseCode, response.response)
 
         if (!isSuccess(response.responseCode) || response.response == null) {
             return WebPaymentResponse(response.responseCode)
