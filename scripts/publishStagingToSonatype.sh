0a7f40b16faf6f278a75550f895d499fb9f800b5
Sergiy Khomyn <sergiy.khomyn@aptoide.com>
2024-06-18 11:59:00 +0100

    Revert unnecessary changes.

--- scripts/publishStagingToSonatype.sh
+++ scripts/publishStagingToSonatype.sh
@@ -4,6 +4,6 @@ cd ..
 export BUILD_NAME_SUFFIX=".staging-SNAPSHOT"
 export BUILD_TYPE_NAME="release"
 
-./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean :appcoins-adyen:clean :communication:clean :appcoins-lifecycle:clean :appcoins-core:clean :appcoins-contract-proxy:clean :appcoins-in-game-updates:clean
-./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble :appcoins-adyen:assemble :communication:assemble :appcoins-lifecycle:assemble  :appcoins-core:assemble :appcoins-contract-proxy:assemble :appcoins-in-game-updates:assemble
+./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean :appcoins-adyen:clean :communication:clean :appcoins-lifecycle:clean :appcoins-core:clean :appcoins-contract-proxy:clean
+./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble :appcoins-adyen:assemble :communication:assemble :appcoins-lifecycle:assemble  :appcoins-core:assemble :appcoins-contract-proxy:assemble
 ./gradlew publish
