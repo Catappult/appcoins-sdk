f4e6d2b5c785d8089861c41e1ddee9603ba27435
SergiyKApt <sergiy.khomyn@aptoide.com>
2024-08-02 14:45:37 +0100

    Remove appcoins-lifecycle module.

--- scripts/publishReleaseToMavenLocal.sh
+++ scripts/publishReleaseToMavenLocal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd ..
 
+# Add '.dev' on the version name. Non dev builds are done though jenkins build only
 export BUILD_TYPE_NAME="release"
 
-./gradlew :android-appcoins-billing:clean :appcoins-billing:clean :communication:clean :appcoins-lifecycle:clean :appcoins-core:clean
-./gradlew :android-appcoins-billing:assembleRelease :appcoins-billing:assembleRelease :communication:assembleRelease :appcoins-lifecycle:assembleRelease :appcoins-core:assembleRelease
+./gradlew :android-appcoins-billing:clean :appcoins-billing:clean :communication:clean :appcoins-core:clean
+./gradlew :android-appcoins-billing:assembleRelease :appcoins-billing:assembleRelease :communication:assembleRelease :appcoins-core:assembleRelease
 ./gradlew publishToMavenLocal
