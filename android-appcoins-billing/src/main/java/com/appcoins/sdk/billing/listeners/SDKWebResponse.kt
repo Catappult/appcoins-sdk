57ba3726512c8ee191ce93cc905c5048a05686ff
SergiyKApt <sergiy.khomyn@aptoide.com>
2024-08-14 18:18:53 +0100

    Fix Web Response parsing.

--- android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/listeners/SDKWebResponse.kt
+++ android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/listeners/SDKWebResponse.kt
@@ -1,9 +1,11 @@
 package com.appcoins.sdk.billing.listeners
 
+import android.app.Activity
 import android.content.Intent
 import com.appcoins.sdk.billing.utils.AppcoinsBillingConstants.INAPP_DATA_SIGNATURE
 import com.appcoins.sdk.billing.utils.AppcoinsBillingConstants.INAPP_PURCHASE_DATA
 import com.appcoins.sdk.billing.utils.AppcoinsBillingConstants.INAPP_PURCHASE_ID
+import com.appcoins.sdk.core.logger.Logger.logInfo
 import org.json.JSONObject
 import java.io.Serializable
 import com.appcoins.sdk.billing.utils.AppcoinsBillingConstants.ORDER_REFERENCE as ORDER_REFERENCE_EXTRA
@@ -17,7 +19,7 @@ data class SDKWebResponse(
 ) {
     constructor(jsonObject: JSONObject) : this(
         jsonObject.optInt(RESPONSE_CODE),
-        PurchaseData(jsonObject.optJSONObject(PURCHASE_DATA) ?: JSONObject()),
+        PurchaseData(JSONObject(jsonObject.optString(PURCHASE_DATA))),
         jsonObject.optString(DATA_SIGNATURE),
         jsonObject.optString(ORDER_REFERENCE),
     )
@@ -27,15 +29,30 @@ data class SDKWebResponse(
     )
 
     fun toSDKPaymentResponse(): SDKPaymentResponse =
-        SDKPaymentResponse(responseCode, createPaymentResponseBundle())
+        SDKPaymentResponse(
+            createActivityResultFromResponseCode(responseCode),
+            createPaymentResponseBundle()
+        )
 
     private fun createPaymentResponseBundle() =
         Intent().apply {
+            logInfo("Putting RESPONSE_CODE_EXTRA with -> ${responseCode}")
+            logInfo("Putting INAPP_PURCHASE_DATA with -> ${purchaseData?.toJson()}")
+            logInfo("Putting INAPP_DATA_SIGNATURE with -> ${dataSignature}")
+            logInfo("Putting INAPP_PURCHASE_ID with -> ${purchaseData?.purchaseToken}")
+            logInfo("Putting ORDER_REFERENCE_EXTRA with -> ${orderReference}")
             putExtra(RESPONSE_CODE_EXTRA, responseCode)
             purchaseData?.toJson()?.let { putExtra(INAPP_PURCHASE_DATA, it) }
             dataSignature?.let { putExtra(INAPP_DATA_SIGNATURE, it) }
             purchaseData?.purchaseToken?.let { putExtra(INAPP_PURCHASE_ID, it) }
             orderReference?.let { putExtra(ORDER_REFERENCE_EXTRA, it) }
+        }
+
+    private fun createActivityResultFromResponseCode(responseCode: Int) =
+        when (responseCode) {
+            0, 2, 3, 4, 5, 6, 7, 8 -> Activity.RESULT_OK
+            1 -> Activity.RESULT_CANCELED
+            else -> Activity.RESULT_FIRST_USER
         }
 
     private companion object {
@@ -58,15 +75,7 @@ data class PurchaseData(
     val developerPayload: String?
 ) : Serializable {
     fun toJson(): String =
-        """{"orderId":"$orderId",
-            |"packageName":"$packageName"
-            |"productId":"$productId"
-            |"productType":"$productType"
-            |"purchaseTime":$purchaseTime
-            |"purchaseToken":"$purchaseToken"
-            |"purchaseState":$purchaseState
-            |"isAutoRenewing":"$isAutoRenewing"
-            |"developerPayload":"$developerPayload"}""".trimMargin()
+        """{"orderId":"$orderId","packageName":"$packageName","productId":"$productId","productType":"$productType","purchaseTime":$purchaseTime,"purchaseToken":"$purchaseToken","purchaseState":$purchaseState,"isAutoRenewing":"$isAutoRenewing","developerPayload":"$developerPayload"}"""
 
     constructor(jsonObject: JSONObject) : this(
         jsonObject.optString(ORDER_ID),
