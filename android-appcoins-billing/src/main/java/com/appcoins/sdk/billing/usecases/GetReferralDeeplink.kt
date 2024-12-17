dadf889acd4ed3d3ba998f046efc17ee509442cf
SergiyKApt <sergiy.khomyn@aptoide.com>
2024-12-17 14:38:52 +0000

    Clean code.

--- android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/usecases/GetReferralDeeplink.kt
+++ android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/usecases/GetReferralDeeplink.kt
@@ -4,6 +4,7 @@ import com.appcoins.sdk.billing.ReferralDeeplink
 import com.appcoins.sdk.billing.ResponseCode
 import com.appcoins.sdk.billing.helpers.WalletUtils
 import com.appcoins.sdk.billing.managers.StoreLinkMapperManager
+import com.appcoins.sdk.core.logger.Logger.logInfo
 
 object GetReferralDeeplink : UseCase() {
 
@@ -15,6 +16,12 @@ object GetReferralDeeplink : UseCase() {
         super.invokeUseCase()
 
         val referralDeeplinkResponse = storeLinkMapperManager.getReferralDeeplink()
+
+        logInfo(
+            "Received ReferralDeeplink. ResponseCode: ${referralDeeplinkResponse.responseCode} " +
+                "| StoreDeeplink: ${referralDeeplinkResponse.storeDeeplink} " +
+                "| FallbackDeeplink: ${referralDeeplinkResponse.fallbackDeeplink}"
+        )
 
         return ReferralDeeplink(
             ResponseCode.fromValue(referralDeeplinkResponse.responseCode) ?: ResponseCode.ERROR,
--- appcoins-billing/src/main/java/com/appcoins/sdk/billing/ReferralDeeplink.kt
+++ appcoins-billing/src/main/java/com/appcoins/sdk/billing/ReferralDeeplink.kt
@@ -3,7 +3,7 @@ package com.appcoins.sdk.billing
 /**
  * @param responseCode with the possible values of [ResponseCode].
  * @param storeDeeplink which is the Deeplink for the Application Store.
- * @param fallbackDeeplink which is the Deeplink for a Fallback Store in
+ * @param fallbackDeeplink which is the Web Link for a Fallback Store in
  * case the storeDeeplink isn't installed in the Device.
  */
 class ReferralDeeplink(
